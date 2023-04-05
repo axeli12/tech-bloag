9ffb9c7902a33e1da6fb64f67a9d92d85e50285d
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-04-04 21:03:12 -0600

    added table data to models

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const  apiRoute = require('./api')
+const home = require('./homeroutes')
 
-router.use('/', apiRoute);
+router.use('/', home);
+router.use('/api', apiRoute);
 
 router.use((req, res) => {
     res.render('404')
